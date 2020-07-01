e028d412994e7dfa16f516462199f83dc44772ea
Eric Liu <liu.p.eric@gmail.com>
2020-06-30 22:37:48 -0700

    recap exercises pt 1

---
+++ software_engineering_foundations/recap_exercise_1/lib/problems.rb
@@ -0,0 +1,132 @@
+# Write a method, all_vowel_pairs, that takes in an array of words and returns all pairs of words
+# that contain every vowel. Vowels are the letters a, e, i, o, u. A pair should have its two words
+# in the same order as the original array. 
+#
+# Example:
+#
+# all_vowel_pairs(["goat", "action", "tear", "impromptu", "tired", "europe"])   # => ["action europe", "tear impromptu"]
+def all_vowel_pairs(words)
+    vowels = "aeiou"
+    vowel_pairs = []
+
+    words.each_with_index do |word, idx|
+        (idx + 1..words.length - 1).each do |i|
+        pair = words[idx] + " " + words[i]
+            vowel_pair = true
+            vowels.each_char do |char|
+                vowel_pair = false if !pair.include?(char)     
+            end
+            vowel_pairs << pair if vowel_pair == true
+        end
+    end
+    vowel_pairs
+end
+
+
+# Write a method, composite?, that takes in a number and returns a boolean indicating if the number
+# has factors besides 1 and itself
+#
+# Example:
+#
+# composite?(9)     # => true
+# composite?(13)    # => false
+def composite?(num)
+    (2...num).each do |divisor|
+        if num % divisor == 0
+            return true
+        end
+    end
+    false
+end
+
+
+
+# A bigram is a string containing two letters.
+# Write a method, find_bigrams, that takes in a string and an array of bigrams.
+# The method should return an array containing all bigrams found in the string.
+# The found bigrams should be returned in the the order they appear in the original array.
+#
+# Examples:
+#
+# find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
+# find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
+def find_bigrams(str, bigrams)
+    bigrams.select { |bigram| str.include?(bigram)}
+end
+
+class Hash
+    # Write a method, Hash#my_select, that takes in an optional proc argument
+    # The method should return a new hash containing the key-value pairs that return
+    # true when passed into the proc.
+    # If no proc is given, then return a new hash containing the pairs where the key is equal to the value.
+    #
+    # Examples:
+    #
+    # hash_1 = {x: 7, y: 1, z: 8}
+    # hash_1.my_select { |k, v| v.odd? }          # => {x: 7, y: 1}
+    #
+    # hash_2 = {4=>4, 10=>11, 12=>3, 5=>6, 7=>8}
+    # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
+    # hash_2.my_select                            # => {4=>4}
+    def my_select(&prc)
+        prc ||= Proc.new { |k,v| k == v}
+        new_hash = {}
+
+        self.each do |key, val| 
+            if prc.call(key, val)
+                new_hash[key] = val
+            end
+        end
+        new_hash
+    end
+end
+
+class String
+    # Write a method, String#substrings, that takes in a optional length argument
+    # The method should return an array of the substrings that have the given length.
+    # If no length is given, return all substrings.
+    #
+    # Examples:
+    #
+    # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
+    # "cats".substrings(2)  # => ["ca", "at", "ts"]
+    def substrings(length = nil)
+        substring_array = []
+
+        self.each_char.with_index do |char, idx|
+            substring = ""
+            (idx...self.length).each do |i|
+                substring += self[i]
+                substring_array << substring
+            end
+        end
+
+        if length.nil?
+            substring_array
+        else
+            substring_array.select { |substring| substring.length == length}
+        end
+    end
+
+
+    # Write a method, String#caesar_cipher, that takes in an a number.
+    # The method should return a new string where each char of the original string is shifted
+    # the given number of times in the alphabet.
+    #
+    # Examples:
+    #
+    # "apple".caesar_cipher(1)    #=> "bqqmf"
+    # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
+    # "zebra".caesar_cipher(4)    #=> "difve"
+    def caesar_cipher(num)
+        alphabet = ("a".."z").to_a
+
+        new_str = self.split("").map.with_index do |char, idx|
+            new_idx = (alphabet.index(char) + num) % 26
+            self[idx] = alphabet[new_idx]
+        end
+        new_str.join("")
+    end
+end
+
+
